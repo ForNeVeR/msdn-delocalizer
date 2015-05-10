a698e4694c06bbe0507c3310c5945da2839671ab
Friedrich von Never <friedrich@fornever.me>
2015-05-10 16:29:05 +0600

    Build with broswerify.

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,5 @@
 /// <reference path="../typings/tsd.d.ts"/>
-import UrlUtils = require('url-utils');
+import UrlUtils = require('./url-utils');
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 	if (changeInfo.status === 'loading') {
