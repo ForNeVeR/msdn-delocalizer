a3bc21dbe769efff0d3a794f9ae77277a78df848
alwaysplaythavictim <woohii8420548@gmail.com>
2024-11-07 21:35:06 +0300

    Added Vite as a build system

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,11 @@
 ///<reference path="../node_modules/@types/chrome/index.d.ts"/>
 
-import UrlUtils = require("./url-utils");
+import { delocalizeUrl } from './url-utils';
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
 	if (changeInfo.url == null) return;
 	const tabUrl = new URL(changeInfo.url);
-	const replacementUrl = UrlUtils.delocalizeUrl(tabUrl);
+	const replacementUrl = delocalizeUrl(tabUrl);
 	if (replacementUrl == null || replacementUrl.toString() == tabUrl.toString()) return;
 	chrome.tabs.update(tabId, {
 		url: replacementUrl.toString()
---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from "vite";
+import webExtension from "vite-plugin-web-extension";
+
+export default defineConfig({
+  plugins: [webExtension()],
+});
