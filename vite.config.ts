ced2f059e1148a9737ec5792d35b2ae62e20988e
alwaysplaythavictim <woohii8420548@gmail.com>
2024-11-19 18:55:17 +0300

    Prettified vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,21 @@
 import { defineConfig } from "vite";
-import * as path from "path";
-import { normalizePath } from "vite";
 import webExtension from "vite-plugin-web-extension";
-import * as fs from "fs";
-import { zip } from "zip-a-folder";
+import {
+    copyLicenceFileToDestFolder,
+    resolvePath,
+    destFolder,
+    createWebExtensionZipFile,
+    cleanupDestFolder,
+} from "./src/build-utils";
 
 const targetBrowser = process.env.TARGET || "chrome";
 const isBuild = process.env.NODE_ENV === "production";
 const isPublish = process.env.PUBLISH === "1";
 
-// normalize path for windows compatibility
-const resolvePath = (...args: string[]) => normalizePath(path.resolve(...args));
-const joinPath = (...args: string[]) => normalizePath(path.join(...args));
-
-const distFolder = resolvePath("build", "dist");
-
 export default defineConfig({
     root: "src",
     build: {
-        outDir: distFolder,
+        outDir: destFolder,
         emptyOutDir: true,
     },
     test: {
@@ -30,33 +27,18 @@ export default defineConfig({
             browser: targetBrowser,
             disableAutoLaunch: true,
             onBundleReady: async () => {
-                const distFolder = resolvePath("build", "dist");
-                const webExtFolder = resolvePath("build", "web-ext");
-
-                await fs.promises.copyFile(
-                    resolvePath("LICENSE.md"),
-                    joinPath(distFolder, "LICENSE.md")
-                );
+                // dev, build and publish post-actions
+                await copyLicenceFileToDestFolder();
 
                 if (!isBuild) return;
 
-                fs.access(webExtFolder, async (error) => {
-                    if (!error) return;
-
-                    await fs.promises.mkdir(webExtFolder);
-                });
-
-                const webExtFilename = `msdn-delocalizer-${targetBrowser}.zip`;
-                const webExtPath = joinPath(webExtFolder, webExtFilename);
-
-                await zip(distFolder, webExtPath);
+                // build and publish post-actions
+                await createWebExtensionZipFile(targetBrowser);
 
                 if (!isPublish) return;
 
-                await fs.promises.rm(distFolder, {
-                    recursive: true,
-                    force: true,
-                });
+                // publish post-actions
+                await cleanupDestFolder();
             },
         }),
     ],
